93c3d4dc5ff611ebaf38dd8070aaf757af1ccc14
= <=>
2025-02-12 15:33:21 -0800

    gpu stuff, need to profile more. maybe it is just slower for this small model

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # CloneMe.jl
 
-
 I downloaded "all" my livestream transcripts and I am training some basic language models on them. I am using Julia and `Flux` for gradients and optimization.
 
 I also have another repo where I worked through more of the math following the "Neural Network Zero to Hero" series:
--- test/clean.jl
+++ test/clean.jl
@@ -2,6 +2,10 @@ using Flux
 using Flux: logitcrossentropy, onehotbatch
 using CloneMe
 using Plots
+using BenchmarkTools
+using CUDA, cuDNN
+CUDA.allowscalar(false)
+device = gpu_device()
 
 s = read("C:/Users/anand/src/transcript_grabber/data/dataset.txt", String)
 na = filter(!isascii, s)
@@ -38,39 +42,44 @@ reshape_fn = x -> reshape(x, (n_embd * block_size, :))
 l1 = Dense(n_embd * block_size, nh, tanh)
 l2 = Dense(nh, nc)
 
-model = Chain(
+model_cpu = Chain(
     emb,
     x -> reshape(x, (n_embd * block_size, :)),
     l1,
     l2
-)
+) 
+model_gpu = deepcopy(model_cpu) |> device
+
+model = model_gpu
 
 # train 
 batch_size = 64
 loader = Flux.DataLoader((data=X, label=Y), batchsize=batch_size, shuffle=true)
 loader2 = Flux.DataLoader((data=X, label=Y), batchsize=batch_size, shuffle=false)
-x1l, y1l = first(loader2)
+xb, yb = first(loader2)
 
 lr = 1e-2
 opt_state = Flux.setup(Flux.Descent(lr), model)
 
 losses = []
 test_losses = []
-for (i, (x, y)) in enumerate(loader)
+for (i, xy_cpu) in enumerate(loader)
+    x, y = xy_cpu |> device
     loss, grads = Flux.withgradient(model) do m
         logits = m(x)
         loss = logitcrossentropy(logits, onehotbatch(y, 1:nc))
     end
+    Flux.update!(opt_state, model, grads[1])
+
     push!(losses, loss)
     if i % 1000 == 0
         test_loss = logitcrossentropy(model(Xv), onehotbatch(Yv, 1:nc))
         push!(test_losses, (i, test_loss))
         @show i loss test_loss
     end
-    # if i == 1000
+    # if i == 10000
     #     break
     # end
-    Flux.update!(opt_state, model, grads[1])
 end
 
 p = plot(losses)
@@ -123,3 +132,90 @@ function embedding_plot(model)
     pl
 end
 embedding_plot(model)
+
+@benchmark model(xb)
+# without CUDA 
+
+
+# julia> @benchmark model(xb)
+# BenchmarkTools.Trial: 10000 samples with 1 evaluation per sample.
+#  Range (min … max):   9.700 μs … 168.900 μs  ┊ GC (min … max): 0.00% … 0.00%
+#  Time  (median):     13.800 μs               ┊ GC (median):    0.00%
+#   ▆▇▆██▇▆▅▄▄▄▃▃▂▂▁▁                       ▁ ▁▁▁                ▂
+#   ████████████████████▇▇▆▇▅▅▃▅▄▅▄▅▅▆▆█▇████████████▇█▆▆▆▅▆▆▅▃▆ █
+#   9.7 μs        Histogram: log(frequency) by time        63 μs <
+
+#  Memory estimate: 25.19 KiB, allocs estimate: 15.
+
+# julia> @benchmark model(xb)
+# BenchmarkTools.Trial: 10000 samples with 1 evaluation per sample.
+#  Range (min … max):   9.500 μs … 196.200 μs  ┊ GC (min … max): 0.00% … 0.00%
+#  Time  (median):     13.300 μs               ┊ GC (median):    0.00%
+#  Time  (mean ± σ):   16.600 μs ±  11.929 μs  ┊ GC (mean ± σ):  0.00% ± 0.00%
+
+#   █▆▇█▇▆▅▅▄▄▃▃▂▁▁▁                                             ▂
+#   █████████████████▇█▇▆▇▆▅▅▅▃▅▆▆▇▇▇██████▇▇▇▆▇▆▇▇▅▆▆▅▄▆▄▆▅▅▅▄▅ █
+#   9.5 μs        Histogram: log(frequency) by time      69.6 μs <
+
+#  Memory estimate: 25.19 KiB, allocs estimate: 15.
+
+xb_gpu = xb |> device
+yb_gpu = yb |> device
+@benchmark model(xb_gpu)
+# with CUDA
+
+# julia> @benchmark model(xb_gpu)
+# BenchmarkTools.Trial: 10000 samples with 1 evaluation per sample.
+#  Range (min … max):  50.000 μs … 805.700 μs  ┊ GC (min … max): 0.00% … 0.00%
+#  Time  (median):     70.900 μs               ┊ GC (median):    0.00%
+#  Time  (mean ± σ):   91.059 μs ±  52.991 μs  ┊ GC (mean ± σ):  0.00% ± 0.00%
+
+#    ▇█▇▇▇▆▅▅▄▄▃▃▃▂▂▂▂▂▂▂▁▁▁▁▂▂▂▂▂▂▁▂▁▁▁                         ▂
+#   ▇████████████████████████████████████▇▇▇▆▇▆▆▇▆▇▇▆▆▆▆▄▆▆▅▆▆▄▅ █
+#   50 μs         Histogram: log(frequency) by time       295 μs <
+
+#  Memory estimate: 9.86 KiB, allocs estimate: 347.
+
+# its like 5 times slower with CUDA
+
+
+
+function update_loss(model, x, y)
+    opt_state = Flux.setup(Flux.Descent(1e-2), model)
+    loss, grads = Flux.withgradient(model) do m
+        logits = m(x)
+        loss = logitcrossentropy(logits, onehotbatch(y, 1:nc))
+    end
+    Flux.update!(opt_state, model, grads[1])
+end
+
+@benchmark update_loss(model_cpu, xb, yb)
+# julia> @benchmark update_loss(model_cpu, xb, yb)
+# BenchmarkTools.Trial: 10000 samples with 1 evaluation per sample.
+#  Range (min … max):  215.800 μs … 548.927 ms  ┊ GC (min … max):  0.00% … 99.83%
+#  Time  (median):     272.000 μs               ┊ GC (median):     0.00%
+#  Time  (mean ± σ):   397.079 μs ±   5.720 ms  ┊ GC (mean ± σ):  25.36% ±  5.51%
+
+#     ▂▅▇█▃          ▁
+#   ▂▅█████▇▅▄▃▃▂▂▃▅███▇▆▅▄▃▃▂▂▂▂▂▂▂▂▂▂▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ ▃
+#   216 μs           Histogram: frequency by time          575 μs <
+
+#  Memory estimate: 364.89 KiB, allocs estimate: 361.
+
+
+@benchmark update_loss(model_gpu, xb_gpu, yb_gpu)
+# julia> @benchmark update_loss(model_gpu, xb_gpu, yb_gpu)
+# BenchmarkTools.Trial: 3363 samples with 1 evaluation per sample.
+#  Range (min … max):  835.700 μs …   4.040 ms  ┊ GC (min … max): 0.00% … 0.00%
+#  Time  (median):       1.409 ms               ┊ GC (median):    0.00%
+#  Time  (mean ± σ):     1.475 ms ± 341.859 μs  ┊ GC (mean ± σ):  0.00% ± 0.00%
+
+#          ▁▂▃▆▄▇▆▇▇▆▇▇▇█▆▅▂▅▅▃▃
+#   ▁▂▂▃▄▄▆████████████████████████▆█▇▇▇▆▆▆▆▄▄▄▄▃▄▃▂▃▃▂▂▂▂▂▂▁▂▁▁▂ ▅
+#   836 μs           Histogram: frequency by time         2.52 ms <
+
+#  Memory estimate: 96.29 KiB, allocs estimate: 2570.
+
+
+CUDA.@profile model_gpu(xb_gpu)
+@benchmark CUDA.@sync model_gpu($xb_gpu)
